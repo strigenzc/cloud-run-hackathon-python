863d0ddca3cadf76c0ea45800e1f117a1e26a823
Catie Strigenz <97646601+strigenzc@users.noreply.github.com>
2022-08-26 13:46:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,7 @@ def index():
 def move():
     request.get_data()
     logger.info(request.json)
-    return moves[{T}.randrange(len(moves))]
+    return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-  
-#testing
